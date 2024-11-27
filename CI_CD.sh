791c8a9d9c2acb6b5deec87605aef890c11d689b
Killian <killian.carvalho-pro@protonmail.com>
2024-11-27 11:49:46 +0100

    add dockerfile request

--- CI_CD.sh
+++ CI_CD.sh
@@ -5,7 +5,7 @@
 set -e
 
 # Variables
-APP_NAME="FlaskApp"
+APP_NAME="Killian_FlaskApp"
 DOCKER_IMAGE_NAME="flaskapp-image"
 DOCKER_CONTAINER_NAME="flaskapp"
 DEV_BRANCH="dev"
@@ -13,8 +13,23 @@ STAGING_BRANCH="staging"
 export PYTHONPATH=$(pwd)
 
 # Get the code from the repository and checkout to dev branch
-git clone git@github.com:devops-ecole89/Killian_DevOps.git $APP_NAME
+git clone git@github.com:devops-ecole89/Killian_FlaskApp.git
+cd $APP_NAME/
 git checkout $DEV_BRANCH
+
+
+# Build and run the docker container
+echo "Build the docker image..."
+docker build -t $DOCKER_IMAGE_NAME .
+docker run -d -p 5000:5000 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+
+# Check if the application is running
+echo "Check if the application is running..."
+curl -s http://localhost:5000/ | grep -q "<h1>Flask App</h1>"
+APP_STATUS=$?
+
+echo "APP_STATUS:"
+echo $APP_STATUS
 
 # Execute the tests
 echo "Execute the tests..."
