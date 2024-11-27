da439c8c97041c583126e22234fef463a775db5f
Killian <killian.carvalho-pro@protonmail.com>
2024-11-27 15:28:49 +0100

    Good for CI_CD.sh

--- CI_CD.sh
+++ CI_CD.sh
@@ -6,33 +6,29 @@ set -e
 
 # Variables
 APP_NAME="Killian_FlaskApp"
-DOCKER_IMAGE_NAME="flaskapp-image"
-DOCKER_CONTAINER_NAME="flaskapp"
 DEV_BRANCH="dev"
 STAGING_BRANCH="staging"
-export PYTHONPATH=$(pwd)
+REPO="git@github.com:devops-ecole89/Killian_FlaskApp.git"
 
-# Get the code from the repository and checkout to dev branch
-git clone git@github.com:devops-ecole89/Killian_FlaskApp.git
-cd $APP_NAME/
+function cleanup {
+  echo "Cleaning up..."
+  cd ..
+  rm -rf $APP_NAME
+}
+
+# Clone the repository
+echo "Cloning the repository..."
+git clone $REPO
+cd $APP_NAME
 git checkout $DEV_BRANCH
-
-
-# Build and run the docker container
-echo "Build the docker image..."
-docker build -t $DOCKER_IMAGE_NAME .
-docker run -d -p 5000:5000 --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
-
-# Check if the application is running
-echo "Check if the application is running..."
-curl -s http://localhost:5000/ | grep -q "<h1>Flask App</h1>"
-APP_STATUS=$?
-
-echo "APP_STATUS:"
-echo $APP_STATUS
 
 # Execute the tests
 echo "Execute the tests..."
+export PYTHONPATH=$(pwd)
+# use venv to install the dependencies
+python3 -m venv venv
+source venv/bin/activate
+pip install --no-cache-dir -r requirements.txt
 pytest tests/
 TEST_STATUS=$?
 
@@ -42,11 +38,11 @@ if [ $TEST_STATUS -eq 0 ]; then
     git checkout $STAGING_BRANCH
     git merge $DEV_BRANCH
     git push origin $STAGING_BRANCH
-    rm -rf $APP_NAME
+    cleanup
     exit 0
 else
     echo "Tests failed"
-    rm -rf $APP_NAME
+    cleanup
     exit 1
 fi
 
---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM python:3.10-slim
+
+# Install the necessary system dependencies
+RUN apt-get update && apt-get install -y \
+    git \
+    build-essential \
+    libssl-dev \
+    libffi-dev \
+    --no-install-recommends && \
+    rm -rf /var/lib/apt/lists/*
+
+# Define the working directory
+WORKDIR /home/python/app
+
+# Clone the Git repository and checkout the dev branch
+RUN git clone https://github.com/devops-ecole89/Killian_FlaskApp.git
+WORKDIR /home/python/app/Killian_FlaskApp
+RUN git checkout dev
+
+# Install the Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Define the PYTHONPATH environment variable to include the code directory
+ENV PYTHONPATH=/home/python/app/Killian_FlaskApp
+
+# Expose the port (optional)
+EXPOSE 5000
+
+# Exécuter l'application
+CMD ["python", "main.py"]
